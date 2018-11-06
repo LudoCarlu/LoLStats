0d919eb3e7cd2c3597a71a84928d68039f05fc1b
Vincent ETIENNE <vincent.eti@gmail.com>
2018-11-06 21:03:34 +0100

    Initial Commit

---
+++ app/src/main/java/com/github/vincebrees/lolstats/ChooseAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.github.vincebrees.lolstats
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.choose_account_activity)
+    }
+}
